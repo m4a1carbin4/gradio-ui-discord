5b4fb38ac66a6075d4fd8ed2020307b7340eeea7
MGM <waganawa@wgnw.monster>
2023-03-18 23:00:51 +0900

    add

--- src/gradio_ui.py
+++ src/gradio_ui.py
@@ -280,6 +280,10 @@ def build_gradio_ui_for(inference_fn, for_kobold):
 
                 if sender == bot.user :
                     return
+                
+                if msg.content.startswith("!"):
+                    await bot.process_commands(message)
+                    return
 
                 nonlocal discord_message , discord_history_for_model , discord_history_for_gradio , discord_chat
 
